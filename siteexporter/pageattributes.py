bb0adb4cc05fb4dfe9942ea71c1ce68a5166c4c7
Marko Mahnič <marko.mahnic@gmail.com>
2018-11-24 22:06:14 +0100

    Add license to python files

--- siteexporter/pageattributes.py
+++ siteexporter/pageattributes.py
@@ -1,3 +1,19 @@
+# vim: set fileencoding=utf-8 sw=4 sts=4 ts=8 et :vim
+# Zim Plugin - Site Exporter
+# Copyright (C) 2018 Marko Mahnič
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import zim.formats
 
 # REQUIRE: pyyaml
